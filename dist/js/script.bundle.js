6d857162723b90bc840ac8897783a4588b2f020f
denis-fominykh <fominykhdenis@gmail.com>
2019-03-28 17:51:05 +0200

    Get and add info about user city

--- dist/js/script.bundle.js
+++ dist/js/script.bundle.js
@@ -97,7 +97,112 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/style.scss */ "./src/scss/style.scss");
 /* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_style_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _js_mvc_mvc__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/mvc/mvc */ "./src/js/mvc/mvc.js");
+/* harmony import */ var _js_mvc_mvc__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_mvc_mvc__WEBPACK_IMPORTED_MODULE_1__);
 
+
+
+/***/ }),
+
+/***/ "./src/js/mvc/mvc.js":
+/*!***************************!*\
+  !*** ./src/js/mvc/mvc.js ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Model =
+/*#__PURE__*/
+function () {
+  function Model() {
+    _classCallCheck(this, Model);
+  }
+
+  _createClass(Model, null, [{
+    key: "getData",
+    value: function getData() {
+      View.createUserCityInfoField();
+      var userCity = document.querySelector('.user-city').value;
+      var url = "http://api.openweathermap.org/data/2.5/weather?q=".concat(userCity.toLowerCase(), "&appid=f49baef6b4984f869cda007be0df0833");
+      var kelvinDegree = 273.15;
+      fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        var userCityTemp = document.querySelector('.city-temp');
+        userCityTemp.innerHTML = "Temperature: ".concat(parseInt(data.main.temp) - kelvinDegree, " C\xB0");
+      });
+    }
+  }]);
+
+  return Model;
+}();
+
+var View =
+/*#__PURE__*/
+function () {
+  function View() {
+    _classCallCheck(this, View);
+  }
+
+  _createClass(View, null, [{
+    key: "createSearchCityField",
+    value: function createSearchCityField() {
+      var app = document.querySelector('.app');
+      var searchBlock = document.createElement('div');
+      searchBlock.setAttribute('class', 'search-block');
+      var userCityInput = document.createElement('input');
+      userCityInput.setAttribute('class', 'user-city');
+      userCityInput.setAttribute('type', 'search');
+      userCityInput.setAttribute('placeholder', 'Type your city');
+      var submitButton = document.createElement('button');
+      submitButton.setAttribute('class', 'submit-button');
+      submitButton.innerHTML = 'Search';
+      app.appendChild(searchBlock);
+      searchBlock.appendChild(userCityInput);
+      searchBlock.appendChild(submitButton);
+    }
+  }, {
+    key: "createUserCityInfoField",
+    value: function createUserCityInfoField() {
+      var app = document.querySelector('.app');
+      var infoBlock = document.createElement('div');
+      infoBlock.setAttribute('class', 'info-block');
+      var cityTemp = document.createElement('p');
+      cityTemp.setAttribute('class', 'city-temp');
+      app.appendChild(infoBlock);
+      infoBlock.appendChild(cityTemp);
+    }
+  }]);
+
+  return View;
+}();
+
+var Controller =
+/*#__PURE__*/
+function () {
+  function Controller() {
+    _classCallCheck(this, Controller);
+  }
+
+  _createClass(Controller, null, [{
+    key: "viewData",
+    value: function viewData() {
+      View.createSearchCityField();
+      var button = document.querySelector('.submit-button');
+      button.addEventListener('click', Model.getData);
+    }
+  }]);
+
+  return Controller;
+}();
+
+Controller.viewData();
 
 /***/ }),
 
---
+++ src/js/mvc/mvc.js
@@ -0,0 +1,62 @@
+class Model {
+  static getData() {
+    View.createUserCityInfoField();
+    let userCity = document.querySelector('.user-city').value;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${userCity.toLowerCase()}&appid=f49baef6b4984f869cda007be0df0833`;
+    const kelvinDegree = 273.15;
+
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        let userCityTemp = document.querySelector('.city-temp');
+        userCityTemp.innerHTML = `Temperature: ${parseInt(data.main.temp) - kelvinDegree} C°`;
+      });
+  }
+}
+
+class View {
+  static createSearchCityField() {
+    let app = document.querySelector('.app');
+
+    let searchBlock = document.createElement('div');
+    searchBlock.setAttribute('class', 'search-block');
+
+    let userCityInput = document.createElement('input');
+    userCityInput.setAttribute('class', 'user-city');
+    userCityInput.setAttribute('type', 'search');
+    userCityInput.setAttribute('placeholder', 'Type your city');
+
+    let submitButton = document.createElement('button');
+    submitButton.setAttribute('class', 'submit-button');
+    submitButton.innerHTML = 'Search';
+
+    app.appendChild(searchBlock);
+    searchBlock.appendChild(userCityInput);
+    searchBlock.appendChild(submitButton);
+  }
+
+  static createUserCityInfoField() {
+    let app = document.querySelector('.app');
+
+    let infoBlock = document.createElement('div');
+    infoBlock.setAttribute('class', 'info-block');
+
+    let cityTemp = document.createElement('p');
+    cityTemp.setAttribute('class', 'city-temp');
+
+    app.appendChild(infoBlock);
+    infoBlock.appendChild(cityTemp);
+  }
+}
+
+class Controller {
+  static viewData() {
+    View.createSearchCityField();
+    let button = document.querySelector('.submit-button');
+    button.addEventListener('click', Model.getData);
+  }
+}
+
+Controller.viewData();
